6c1ee338b0206a4c935f23211b86441b3180e05a
Unterhofer, Sebastian <sebastian.unterhofer@nhm-wien.ac.at>
2025-09-09 16:16:27 +0200

    tests: Move parser fixtures into parser test dir and extract full file strings into actual files.
    Signed-off-by: Unterhofer, Sebastian <sebastian.unterhofer@nhm-wien.ac.at>

--- tests/parser/conftest.py
+++ tests/parser/conftest.py
@@ -17,3 +17,27 @@ def file_edm_record_minimal(files) -> bytes:
 def xml_with_lang_in_edm_type(files) -> bytes:
     with open(files / "xml-with-lang-in-edm-types.xml", "rb") as file:
         return file.read()
+
+
+@fixture(scope="session")
+def xml_with_xsdtypes(files) -> bytes:
+    with open(files / "xml-with-xsdtypes.xml", "rb") as file:
+        return file.read()
+
+
+@fixture(scope="session")
+def ref_lit_xml(files) -> bytes:
+    with open(files / "ref-lit-xml.xml", "rb") as file:
+        return file.read()
+
+
+@fixture(scope="session")
+def xml_with_empty_description(files) -> bytes:
+    with open(files / "xml-with-empty-description.xml", "rb") as file:
+        return file.read()
+
+
+@fixture(scope="session")
+def xml_with_empty_description_and_invalid_ref(files) -> bytes:
+    with open(files / "xml-with-empy-description-and-invalid-ref.xml", "rb") as file:
+        return file.read()
--- tests/parser/test_parser.py
+++ tests/parser/test_parser.py
@@ -3,13 +3,6 @@ import pytest
 from edmlib import EDM_Parser, EDM_Record, Ref
 from pathlib import Path
 from tests.fixtures.record import xml_string  # noqa: F401
-from tests.fixtures.parser import (
-    get_ref_lit_json,
-    get_ref_lit_xml,
-    get_xml_with_xsdtypes,
-    xml_with_empty_description,
-    xml_with_empty_description_and_invalid_ref,
-)
 
 
 def test_file_parser() -> None:
@@ -37,8 +30,8 @@ def test_string_parser(xml_string) -> None:  # noqa: ANN001, F811
     assert EDM_Record.model_validate(rec)
 
 
-def test_serialization_lit_ref(get_ref_lit_xml) -> None:
-    parser = EDM_Parser.from_string(get_ref_lit_xml)
+def test_serialization_lit_ref(ref_lit_xml) -> None:
+    parser = EDM_Parser.from_string(ref_lit_xml)
     rec = parser.parse()
     assert isinstance(rec.provided_cho.dc_type, list)
     assert len(rec.provided_cho.dc_type) == 1
@@ -51,8 +44,8 @@ def test_serialization_lit_ref(get_ref_lit_xml) -> None:
     assert isinstance(re_rec.provided_cho.dc_type[0], Ref)
 
 
-def test_xml_datatypes_parsing(get_xml_with_xsdtypes) -> None:
-    parser = EDM_Parser.from_string(get_xml_with_xsdtypes)
+def test_xml_datatypes_parsing(xml_with_xsdtypes) -> None:
+    parser = EDM_Parser.from_string(xml_with_xsdtypes)
     rec = parser.parse()
     assert rec
 
