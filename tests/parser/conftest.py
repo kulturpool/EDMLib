febd56720d579f42d110a84e0f6dec9f3e2e4d80
Unterhofer, Sebastian <sebastian.unterhofer@nhm-wien.ac.at>
2025-09-09 12:47:06 +0200

    tests: Remove error handling and assertion as error is never thrown. Refactor src file.
    Signed-off-by: Unterhofer, Sebastian <sebastian.unterhofer@nhm-wien.ac.at>

--- tests/parser/conftest.py
+++ tests/parser/conftest.py
@@ -9,5 +9,5 @@ def files_edm():
 
 @fixture(scope="session")
 def file_edm_record_minimal(files_edm) -> bytes:
-    with open(files_edm / "edm-record_minimal.xml", "rb") as file:
+    with open(files_edm / "edm-record_aggregation-uri.xml", "rb") as file:
         return file.read()
--- tests/parser/test_aggregation_uri_validation.py
+++ tests/parser/test_aggregation_uri_validation.py
@@ -1,53 +1,30 @@
-from pydantic import ValidationError
 from edmlib import EDM_Parser, EDM_Record, EDM_ProvidedCHO, ORE_Aggregation, Ref, Lit
 from rdflib import Graph, URIRef
 
 
-def test_parse_xml(file_edm_record_minimal):
-    validation_errors = []
-
-    try:
-        EDM_Parser.from_string(file_edm_record_minimal.decode("utf-8")).parse()
-    except ValidationError as e:
-        validation_errors.extend([error.get("msg") for error in e.errors()])
-
-    assert (
-        "Assertion failed, URIs of providedCHO and aggregation.edm_aggregatedCHO do not match: self.provided_cho.id.value='https://uri.test/edm123%C3%BC' != self.aggregation.edm_aggregatedCHO.value='https://uri.test/edm123ü'."
-        not in validation_errors
-    )
+def test_parse_edm_xml(file_edm_record_minimal):
+    EDM_Parser.from_string(file_edm_record_minimal.decode("utf-8")).parse()
 
 
 def test_construct_programmatically():
-    validation_errors = []
-
-    try:
-        EDM_Record(
-            provided_cho=EDM_ProvidedCHO(
-                id=Ref(value="https://uri.test/edm123ü"),
-                dc_type=[Lit(value="Text", lang="en")],
-                dc_title=[Lit(value="Titel", lang="de")],
-                dc_identifier=[Lit(value="123")],
-                dc_language=[Lit(value="de")],
-                edm_type=Lit(value="TEXT"),
-            ),
-            aggregation=ORE_Aggregation(
-                id=Ref(value="http://uri.test/edm123#Aggregation"),
-                edm_aggregatedCHO=Ref(value="https://uri.test/edm123ü"),
-                edm_dataProvider=Lit(value="Test"),
-                edm_isShownAt=Ref(value="http://uri.test/edm123.jpg"),
-                edm_isShownBy=Ref(value="http://uri.test/edm123.jpg"),
-                edm_provider=Lit(value="Kulturpool"),
-                edm_rights=Ref(
-                    value="http://creativecommons.org/licenses/by-nc-sa/4.0/"
-                ),
-            ),
-        )
-    except ValidationError as e:
-        validation_errors.extend([error.get("msg") for error in e.errors()])
-
-    assert (
-        "Assertion failed, URIs of providedCHO and aggregation.edm_aggregatedCHO do not match: self.provided_cho.id.value='https://uri.test/edm123%C3%BC' != self.aggregation.edm_aggregatedCHO.value='https://uri.test/edm123ü'."
-        not in validation_errors
+    EDM_Record(
+        provided_cho=EDM_ProvidedCHO(
+            id=Ref(value="https://uri.test/edm123ü"),
+            dc_type=[Lit(value="Text", lang="en")],
+            dc_title=[Lit(value="Titel", lang="de")],
+            dc_identifier=[Lit(value="123")],
+            dc_language=[Lit(value="de")],
+            edm_type=Lit(value="TEXT"),
+        ),
+        aggregation=ORE_Aggregation(
+            id=Ref(value="http://uri.test/edm123#Aggregation"),
+            edm_aggregatedCHO=Ref(value="https://uri.test/edm123ü"),
+            edm_dataProvider=Lit(value="Test"),
+            edm_isShownAt=Ref(value="http://uri.test/edm123.jpg"),
+            edm_isShownBy=Ref(value="http://uri.test/edm123.jpg"),
+            edm_provider=Lit(value="Kulturpool"),
+            edm_rights=Ref(value="http://creativecommons.org/licenses/by-nc-sa/4.0/"),
+        ),
     )
 
 
